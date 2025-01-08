18674c0f7c534ce7570b420b91a6b38caa78d639
Nolan West <75007954+nolan7512@users.noreply.github.com>
2025-01-08 12:03:20 +0700

    Update run.py
    update

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import requests
 import time
 from telegram import Bot
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater
 
 # Thay thế bằng token bot của bạn
 TOKEN = '7324758222:AAGCiGsotQ6Y-eVgoXrwPDNRSAP5GFNxsq4'
@@ -30,24 +30,33 @@ def fetch_data():
     for entry in data['context']['ladder']['entries']:
         if entry['character']['name'] == CHARACTER_NAME:
             if entry['dead']:
-                message = f"Nhân vật {CHARACTER_NAME} đã chết."
+                rank = entry['rank']
+                message = (
+                    f"WARNING  WARNING  WARNING  Character {CHARACTER_NAME} is dead. Current rank is {rank}.\n"
+                    f"WARNING  Nhân vật {CHARACTER_NAME} đã chết. Rank hiện tại là {rank}."
+                )
                 message_ids = []
                 for _ in range(5):
                     msg = bot.send_message(chat_id=CHANNEL_ID, text=message)
                     message_ids.append(msg.message_id)
-                # Ghim tin nhắn đầu tiên trong số 5 tin nhắn
+                # Ghim tin nhắn đầu tiên trong số 5 tin nhắn đã gửi
                 bot.pin_chat_message(chat_id=CHANNEL_ID, message_id=message_ids[0])
                 # Ngừng việc fetch dữ liệu
                 continue_fetching = False
             else:
                 rank = entry['rank']
-                message = f"Nhân vật {CHARACTER_NAME} chưa chết. Rank hiện tại là {rank}."
+                message = (
+                    f"Character {CHARACTER_NAME} is not dead. Current rank is {rank}.\n"
+                    f"Nhân vật {CHARACTER_NAME} chưa chết. Rank hiện tại là {rank}."
+                )
                 bot.send_message(chat_id=CHANNEL_ID, text=message)
             break
 
 # Hàm khởi động bot và thiết lập thời gian fetch dữ liệu
-def start(update, context):
-    update.message.reply_text('Bot đã bắt đầu theo dõi nhân vật.')
+def start():
+    global continue_fetching
+    print('Bot đã bắt đầu theo dõi nhân vật.')
+    # Bot đã bắt đầu theo dõi nhân vật.
     while continue_fetching:
         fetch_data()
         time.sleep(60)
@@ -56,8 +65,9 @@ def start(update, context):
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+# Bắt đầu fetch dữ liệu ngay lập tức
+start()
 
+# Khởi động polling để giữ cho bot hoạt động
 updater.start_polling()
 updater.idle()
